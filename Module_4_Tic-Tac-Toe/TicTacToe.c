60c452d540772866f267c2220709a6f169f3478f
cphchip <cphchip@gmail.com>
2022-09-23 13:54:43 -0500

    Push from desktop

--- Module_4_Tic-Tac-Toe/TicTacToe.c
+++ Module_4_Tic-Tac-Toe/TicTacToe.c
@@ -1,22 +1,14 @@
 // TacTacToe solution checker by Chip Henderson for SMU CS7343
 
-#include <pthread.h> /* Need to reneable this when compiling for linux, disable for Windows */
+#include <pthread.h> 
 #include <stdio.h>
 #include <stdlib.h>
-#define num_threads 3 /* Stop gap solution until multithread version is ready */
-// int num_threads = 7;
+#define NUM_THREADS 3 
 
 char gameBoard[9];
-// int solutionArray[num_threads]
-// int solutionArray[8];
 int solutionArray[7];
 // Note: solution array consists of row, row, row, column, column, column, diag, diag
 char winner;
-
-// typedef struct {
-//     int row;
-//     int column;
-// } parameters;
 
 void intro()
 {
@@ -28,14 +20,14 @@ void intro()
 
 }
 
-void *rowCheck ()
+void *rowCheck()
 {
     int i = 0;
     int j;
     
     for (j = 0; j < 10; j+=3)
     {
-        if (gameBoard[j] == gameBoard[j + 1] && gameBoard[j + 1] == gameBoard[j + 3]) {
+        if (gameBoard[j] == gameBoard[j + 1] && gameBoard[j + 1] == gameBoard[j + 2]) {
             solutionArray[i] = gameBoard[j];
         }
             else {
@@ -64,67 +56,35 @@ void *columnCheck()
 
 void *diagCheck()
 {
-/*Note: I think it may be possible to get by with only one diagonal result. Will check later.*/
-    int i;
 
-    // for (i = 6; i < 8; i++)
-    // {
-        if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8]) {
+    if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8]) {
+        solutionArray[6] = gameBoard[4];
+    }
+        else if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6]) {
             solutionArray[6] = gameBoard[4];
+        }       
+        else {
+            solutionArray[6] = 0;
         }
-            else if (gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6]) {
-                solutionArray[6] = gameBoard[4];
-            }       
-            else {
-                solutionArray[6] = 0;
-            }
-    // }
-    
 }
 
 int main()
 {
-    /* Note: This section works single threaded...
-    intro();
-    rowCheck();
-    columnCheck();
-    diagCheck();
-
-    int i;
-    int solutionSum = 0;
-    for (i = 0; i < num_threads; i++) {
-        solutionSum += solutionArray[i];
-    }
-        
-    if (solutionSum == 0) {
-        printf("\nThere is no winner!\n");
-    }
-    else if (solutionSum % 88 == 0) {
-        printf("\nWinner is X!\n");
-    }
-    else printf("\nWinner is O!\n");
-    */
-
-//    Multithreaded section
    intro();
    
-//    pthread_t tid = 0;
-   pthread_t tid[num_threads];
+   pthread_t tid[NUM_THREADS];
    int tIndex = 0;
    pthread_attr_t attr;
 
    pthread_attr_init(&attr);
 
-//    pthread_create(&tid, &attr, rowCheck, NULL);
-//    pthread_create(&tid, &attr, columnCheck, NULL);
-//    pthread_create(&tid, &attr, diagCheck, NULL);
-   
    pthread_create(&tid[tIndex++], &attr, rowCheck, NULL);
    pthread_create(&tid[tIndex++], &attr, columnCheck, NULL);
    pthread_create(&tid[tIndex++], &attr, diagCheck, NULL);
+   
 
     int j;
-    for (j = 0; j < num_threads; j++) {
+    for (j = 0; j < NUM_THREADS; j++) {
 
         pthread_join(tid[j],NULL);
 
@@ -133,6 +93,7 @@ int main()
     int i;
     int solutionSum = 0;
     for (i = 0; i < 7; i++) {
+        printf("\n%d", solutionArray[i]); // This is a debug statement
         solutionSum += solutionArray[i];
     }
         
