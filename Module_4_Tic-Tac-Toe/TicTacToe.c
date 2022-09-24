1cec55f081650fc57602df5f6becd98837b49fbe
cphchip <cphchip@gmail.com>
2022-09-24 09:39:58 -0500

    Updated with comments

--- Module_4_Tic-Tac-Toe/TicTacToe.c
+++ Module_4_Tic-Tac-Toe/TicTacToe.c
@@ -8,12 +8,7 @@
 char gameBoard[8]; 
 int solutionArray[6]; // consists of row, row, row, col, col, col, diag 
 
-/* 
-    Somehow reducing the two values above by 1 each resolved the problem of occaisional
-    incorrect results for columns
-*/
-
-void intro()
+void intro() //Welcomes user and sets up the game with user input
 {
     printf("Welcome to Tac-Tac-toe\n");
     printf("Enter a game board such as XOOOXOOOX\n");
@@ -22,7 +17,7 @@ void intro()
     printf( "\nYou entered: %s", gameBoard);
 }
 
-void *rowCheck()
+void *rowCheck() // Checks each row for possible winner, updates solution array
 {
     int i = 0;
     int j = 0;
@@ -39,7 +34,7 @@ void *rowCheck()
     }
 }
 
-void *columnCheck()
+void *columnCheck() // Checks each column for possible winner, updates solution array
 {
     int i = 3;
     int j = 0;
@@ -56,7 +51,7 @@ void *columnCheck()
     }
 }
 
-void *diagCheck()
+void *diagCheck() // Checks each diagonal for possible winner, updates solution array
 {
     if (gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8]) {
         solutionArray[6] = gameBoard[4];
@@ -71,20 +66,22 @@ void *diagCheck()
 
 int main()
 {
-   intro();
+    intro();
    
-   pthread_t tid[NUM_THREADS];
-   int tIndex = 0;
-   pthread_attr_t attr;
+    // Setup the threads
+    pthread_t tid[NUM_THREADS];
+    int tIndex = 0;
+    pthread_attr_t attr;
 
-   pthread_attr_init(&attr);
+    pthread_attr_init(&attr);
 
-   pthread_create(&tid[0], &attr, rowCheck, NULL);
-   pthread_create(&tid[1], &attr, columnCheck, NULL);
-   pthread_create(&tid[2], &attr, diagCheck, NULL);
+    // Runs each function as it's own thread
+    pthread_create(&tid[0], &attr, rowCheck, NULL);
+    pthread_create(&tid[1], &attr, columnCheck, NULL);
+    pthread_create(&tid[2], &attr, diagCheck, NULL);
    
     int j;
-    for (j = 0; j < NUM_THREADS; j++) {
+    for (j = 0; j < NUM_THREADS; j++) { // Ensures each thread is joined
 
         pthread_join(tid[j],NULL);
 
@@ -92,7 +89,9 @@ int main()
 
     int solutionSum = 0;
     int i;
-    for (i = 0; i < 7; i++) {
+
+    for (i = 0; i < 7; i++) { 
+        // Adds values so winner can be evaluated as single value instead of array
         solutionSum += solutionArray[i];              
     }
         
